b87b135991c9b5cd30bc632909c02eb3080f792c
LeRoiDesKiwis <ferryantony1@gmail.com>
2019-12-24 11:44:07 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const DiscordRPC = require('discord-rpc');
+const rpc = new DiscordRPC.Client({transport: 'ipc'});
+
+function setActivity(){
+    
+    const noel = Date.UTC(2019, 12, 24, 18, 0, 0, 0);
+    const now = Date.now();
+    const difference = new Date(noel-now)
+
+    const seconds = difference.getUTCSeconds();
+    const minutes = difference.getUTCMinutes();
+    const hours = difference.getUTCHours()-1;
+    const days = difference.getUTCDate()-1;
+
+    rpc.setActivity({
+        details: `${days} days ${hours}h ${minutes}min ${seconds}s`,
+        state:'Attends la veille de Noël',
+        largeImageKey: 'kanna',
+    });
+}
+
+rpc.on('ready', () => {
+    console.log('Ready !');
+    setActivity();
+
+    setInterval(() => setActivity(), 1e3)
+})
+
+rpc.login({clientId: '658344804513611777'})
